78e1a21f0634013ffc013ed6cd17b3b8a5b7e30b
Harsh Parmar <159010140+HiamRyuthere@users.noreply.github.com>
2025-12-19 14:06:54 +0530

    Clarify allowed origins comment in WebSocketConfig
    Updated comment for allowed origins in WebSocketConfig.

--- ephernalChat/src/main/java/com/ephernal/ephernalChat/config/WebSocketConfig.java
+++ ephernalChat/src/main/java/com/ephernal/ephernalChat/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) { // 4. The Rulebook
         registry.addHandler(new ChatWebSocketHandler(), "/chat") // 5. The Main Rule
-                .setAllowedOrigins("https://chat.ryuverse.fun/"); // 6. The VIP List
+                .setAllowedOrigins("https://chat.ryuverse.fun/"); // 6. The VIP list.(Change with your web url)
     }
 
 }
